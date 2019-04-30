2f088befc5bb18c93721947d08f1c69c17754965
soleilyasmina <mounlasolomon@gmail.com>
2019-04-30 11:17:46 -0400

    created post

--- routes/article-router.js
+++ routes/article-router.js
@@ -4,13 +4,22 @@ const { Article } = require('../models');
 const articleRouter = express.Router();
 
 articleRouter.get('/', async (req, res) => {
-    try {
-      const articles = await Article.findAll();
-      res.send(articles);
-    } catch (e) {
-      res.status(500).json({ msg: e.message });
-    }
+  try {
+    const articles = await Article.findAll();
+    res.send(articles);
+  } catch (e) {
+    res.status(500).json({ msg: e.message });
+  }
 });
+
+articleRouter.post('/', async (req, res) => {
+  try {
+    const article = await Article.create(req.body);
+    res.send(article);
+  } catch (e) {
+    res.status(500).json({ msg: e.message });
+  }
+})
 
 module.exports = {
   articleRouter
