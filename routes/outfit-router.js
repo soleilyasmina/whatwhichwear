2d941596593d3a72fbdbdbc2695aff93b8c4d454
soleilyasmina <mounlasolomon@gmail.com>
2019-04-30 13:50:59 -0400

    added put and delete

--- routes/outfit-router.js
+++ routes/outfit-router.js
@@ -30,6 +30,26 @@ outfitRouter.get('/:id', async (req, res) => {
   }
 });
 
+outfitRouter.put('/:id', async (req, res) => {
+  try {
+    const outfit = await Outfit.findByPk(req.params.id);
+    await outfit.update(req.body);
+    res.send(outfit);
+  } catch (e) {
+    res.status(500).json({ msg: e.message });
+  }
+});
+
+outfitRouter.delete('/:id', async (req, res) => {
+  try {
+    const outfit = await Outfit.findByPk(req.params.id);
+    await outfit.destroy();
+    res.send(outfit);
+  } catch (e) {
+    res.status(500).json({ msg: e.message });
+  }
+})
+
 module.exports = {
   outfitRouter
 }
