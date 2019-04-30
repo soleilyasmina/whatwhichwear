c7115959198812b000fd696c0bb061980416fb45
soleilyasmina <mounlasolomon@gmail.com>
2019-04-30 00:27:36 -0400

    added dependencies

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3001 || process.env.PORT;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
   try {
