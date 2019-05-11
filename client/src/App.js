58254a1c983b20a772384a8d2aaeddcc95ec551e
soleilyasmina <mounlasolomon@gmail.com>
2019-05-10 22:04:01 -0400

    loaded articles into state and rendered ArticleList

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import ArticleList from './components/ArticleList/ArticleList';
 import './App.css';
 
 function App() {
   const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      let resp = await axios.get('http://localhost:3001/articles');
+      setArticles(resp.data);
+    }
+    getArticles();
+  }, []);
 
   return (
     <div className="App">
