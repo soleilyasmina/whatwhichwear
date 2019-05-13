04ba11fd9f9690136c7071ab170dee7aa9084428
soleilyasmina <mounlasolomon@gmail.com>
2019-05-13 10:35:32 -0400

    added details and styled

--- client/src/components/ArticleDetail/ArticleDetail.js
+++ client/src/components/ArticleDetail/ArticleDetail.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './ArticleDetail.css';
 
-const ArticleDetail = props => {
+const ArticleDetail = ({ article }) => {
   return (
-    <h1>Here's a Detail!</h1>
+    <div className="ArticleDetail">
+      <img src={require('../../assets/cat.png')} alt={article.description}/>
+      <h3>{`${article.color} ${article.brand} ${article.type}`}</h3>
+      <p>{article.description}</p>
+      <p>You've worn this {article.times_worn} times.</p>
+      <p>Fit: {article.fit}</p>
+      <p>Season: {article.season}</p>
+      <p>Occasion: {article.occasion}</p>
+    </div>
   )
 }
 
