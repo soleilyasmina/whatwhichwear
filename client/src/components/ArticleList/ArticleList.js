43599d44ffc2edbd3b151a5d6d6a6a000472b340
soleilyasmina <mounlasolomon@gmail.com>
2019-05-13 09:10:28 -0400

    passed down setCurrentArticle

--- client/src/components/ArticleList/ArticleList.js
+++ client/src/components/ArticleList/ArticleList.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import ArticleItem from '../ArticleItem/ArticleItem';
 
-const ArticleList = ({ articles }) => {
+const ArticleList = ({ articles, setCurrentArticle }) => {
   return (
     <Fragment>
       { articles.map((article, index) => {
         return (
-          <ArticleItem key={index} article={article} />
+          <ArticleItem key={index} article={article} setCurrentArticle={setCurrentArticle} />
         )
       }) } 
     </Fragment>
